3e7243a6d95504438a341ce9685e1019121a6661
Future_Gohan <s1841390@online.houstonisd.org>
2026-01-22 21:27:11 -0600

    1/22 bugfixes for visionsystem.java
    Added optional constructor and setters for camera mount info.

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,11 +1,20 @@
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.targeting.TargetCorner;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionSubsystem extends SubsystemBase {
 
@@ -15,22 +24,22 @@ public class VisionSubsystem extends SubsystemBase {
     
     private boolean hasTarget = false;
     
-    //idk if this is private but i will just put it here
+    // idk if this is private but i will just put it here
     private PhotonTrackedTarget target;
     
     private double targetYaw = 0.0;
     private double targetPitch = 0.0;
     private double targetArea = 0.0;
     private double targetSkew = 0.0;
-    List<TargetCorner> targetCorners = new ArrayList<>();
-    double poseAmbiguity = 0.0;
+    private List<TargetCorner> targetCorners = new ArrayList<>();
+    private double poseAmbiguity = 0.0;
 
-    //When camera is mounted, set these variables
-    private Pose3d cameraToRobot;
-    private final double cameraHeightMeters;
-    private double cameraPitchRadians;
+    // When camera is mounted, set these variables
+    private Transform3d cameraToRobot = null;
+    private double cameraHeightMeters = 0.0;
+    private double cameraPitchRadians = 0.0;
 
-    //Pipeline Indexes. Dont know if we will these but im putting them here just in case
+    // Pipeline Indexes. Dont know if we will use these but im putting them here just in case
     public static final int APRILTAG_PIPELINE = 0;
     public static final int TAPE_PIPELINE = 1;
     public static final int DRIVER_PIPELINE = 2;
@@ -41,8 +50,15 @@ public class VisionSubsystem extends SubsystemBase {
         camera = new PhotonCamera(cameraName);
     }
 
-    
-    //Pipeline setters and getters, not sure if we will actually use these.
+    // Optional overload to supply camera-to-robot transform at construction
+    public VisionSubsystem(String cameraName, Transform3d cameraToRobot, double cameraHeightMeters, double cameraPitchRadians) {
+        camera = new PhotonCamera(cameraName);
+        this.cameraToRobot = cameraToRobot;
+        this.cameraHeightMeters = cameraHeightMeters;
+        this.cameraPitchRadians = cameraPitchRadians;
+    }
+
+    // Pipeline setters and getters, not sure if we will actually use these.
     public void setPipeline(int index) {
         camera.setPipelineIndex(index);
     }
@@ -106,26 +122,39 @@ public class VisionSubsystem extends SubsystemBase {
         return targetID;
     }
 
-    //Checks how reliable the target info is based on poseAmbiguity
+    // Checks how reliable the target info is based on poseAmbiguity
     public boolean hasReliablePose(double maxAmbiguity) {
         return hasTarget && poseAmbiguity >= 0 && poseAmbiguity < maxAmbiguity;
     }
 
     public Optional<Transform3d> getBestCameraToTarget() {
-        return hasTarget ? Optional.of(target.getBestCameraToTarget()) : Optional.empty();
+        return hasTarget ? Optional.ofNullable(target.getBestCameraToTarget()) : Optional.empty();
     }
     
     public Optional<Transform3d> getAlternateCameraToTarget() {
-        return hasTarget ? Optional.of(target.getAlternateCameraToTarget()) : Optional.empty();
+        return hasTarget ? Optional.ofNullable(target.getAlternateCameraToTarget()) : Optional.empty();
     }
     
 
     public Optional<Pose3d> getFieldRelativePose() {
-        if (!hasTarget) {
+        if (!hasTarget || cameraToRobot == null) {
             return Optional.empty();
         }
         
-        return aprilTagFieldLayout.getTagPose(targetID).map(tagPose -> PhotonUtils.estimateFieldToRobotAprilTag(target.getBestCameraToTarget(),tagPose,cameraToRobot));
+        return aprilTagFieldLayout.getTagPose(targetID).map(tagPose -> PhotonUtils.estimateFieldToRobotAprilTag(target.getBestCameraToTarget(), tagPose, cameraToRobot));
+    }
+
+    // setters for camera mount info
+    public void setCameraToRobot(Transform3d cameraToRobot) {
+        this.cameraToRobot = cameraToRobot;
+    }
+
+    public void setCameraHeightMeters(double cameraHeightMeters) {
+        this.cameraHeightMeters = cameraHeightMeters;
+    }
+
+    public void setCameraPitchRadians(double cameraPitchRadians) {
+        this.cameraPitchRadians = cameraPitchRadians;
     }
 
     // We Need cameraHeightMeters and cameraPitchRadians to calculate distance to target
@@ -134,19 +163,10 @@ public class VisionSubsystem extends SubsystemBase {
     
     /* 
     public double calcDistanceToTarget(double cameraHeightMeters, double targetHeightMeters, double cameraPitchRadians, double targetPitchRadians) {
-        return PhotonUtils.calculateDistanceToTargetMeters(cameraHeightMeters, targetHeightMeters, cameraPitchRadians, targetPitchRadians)
+        return PhotonUtils.calculateDistanceToTargetMeters(cameraHeightMeters, targetHeightMeters, cameraPitchRadians, targetPitchRadians);
     }
     */
     
     
     
 }
-
-
-
-
-
-
-
-
-
