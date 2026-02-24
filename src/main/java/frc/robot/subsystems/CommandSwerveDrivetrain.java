00b45e802e9ca9992d339c9cea5da209e89accee
Future_Gohan <s1841390@online.houstonisd.org>
2026-02-23 18:46:11 -0600

    starting pathplanning

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -20,6 +20,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
@@ -286,6 +287,10 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     //get robot pose
     public Pose2d getPose() {
         return getState().Pose;
+    }
+
+    public ChassisSpeeds getSpeeds() {
+        return getState().Speeds;
     }
 
     @Override
